e446df792c99d58864cff6c8f74563c5fa76e6db
pranay <pranaykawale500@gmail.com>
2021-10-20 09:38:11 +0530

    Added

---
+++ src/com/bridgelab/Gambler.java
@@ -0,0 +1,17 @@
+package com.bridgelab;
+
+public class Gambler {
+
+
+	//Main method
+	public static void main (String[]args){
+		gamblerStatus();  //gamblerStatus() method is called
+	}
+
+	static void gamblerStatus() {
+		int stakePerDay = 100; //stake is in dollar
+		int betPerGame =  1 ; 
+
+	} 
+
+}
